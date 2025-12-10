0c55349b602251cde96c7cdd0987d497a61ed0a8
Bhargavi Kurukunda <bhargavi_kurukunda@ucsb.edu>
2025-12-09 17:10:35 -0800

    vercel json updated

--- web/vite.config.js
+++ web/vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: { histroyApiFallback: true }
   // base: "/",  // IMPORTANT: reset this
 });
